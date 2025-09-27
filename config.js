ec044d53bd45bb35466bc3d6dcaf6ed30700c5fc
田中一平 <121410196+ippeitanaka@users.noreply.github.com>
2025-09-27 10:41:31 +0000

    Add config.js to root

---
+++ config.js
@@ -0,0 +1,3 @@
+// This file is generated at build time. Do not edit.
+window.SUPABASE_URL = "https://foqmymtpnkffbuywpmwq.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvcW15bXRwbmtmZmJ1eXdwbXdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwODA4NzksImV4cCI6MjA3MzY1Njg3OX0.SHI3aJJTP9VJkUMHQSIcxaLA3-0FUpVvf5ZRgcmOZa4";
